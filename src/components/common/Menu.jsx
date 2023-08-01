e7ad55bbe5d2de21767ab03d0d5575543484a525
Emilse Arias <emilsearias@gmail.com>
2023-07-31 21:04:01 -0300

    agregamos las rutas a las opciones del menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,19 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink, Link } from "react-router-dom";
 
 
 const Menu = () => {
   return (
     <Navbar bg="danger" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+        <Navbar.Brand as={Link} to={'/'}>Cafecito</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Registro</Nav.Link>
-            <Nav.Link href="#">Administrador</Nav.Link>
-            <Nav.Link href="#">Login</Nav.Link>
+            <NavLink end to='/' className='nav-link'>Inicio</NavLink>
+            <NavLink end to='/registro' className='nav-link'>Registro</NavLink>
+            <NavLink end to='/administrador' className='nav-link'>Administrador</NavLink>
+            <NavLink end to='/login' className='nav-link'>Login</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
