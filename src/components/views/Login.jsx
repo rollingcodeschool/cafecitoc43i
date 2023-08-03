c754d5b8c48ba48c176894e1c3933b9e88e048c2
Emilse Arias <emilsearias@gmail.com>
2023-08-02 21:36:09 -0300

    agregamos validaciones al formulario de login usando la libreria de react-hook-form

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,21 +1,45 @@
 
 import { Form, Button, Container, Card } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState:{errors}
+  }= useForm();
+
+  // esta es mi funcion la que pide loguear al usuario
+  const onSubmit = (usuario)=>{
+    console.log('aqui agrego mi logica')
+    console.log(usuario)
+  }
+
   return (
     <Container className="mainSection">
       <Card className="my-5">
         <Card.Header as="h5">Login</Card.Header>
         <Card.Body>
-          <Form >
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="Ingrese un email"
-               
+                {
+                  ...register('email',{
+                    required: 'El email es un dato obligatorio',
+                    pattern:{
+                      value:/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                      message:'El email debe cumplir con un formato valido como el siguiente mail@mail.com '
+                    }
+                  })
+                }
               />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
              
             </Form.Group>
 
@@ -24,9 +48,19 @@ const Login = () => {
               <Form.Control
                 type="password"
                 placeholder="Password"
-               
+               {
+                ...register('password',{
+                  required:'El password es un dato obligatorio',
+                  pattern: {
+                    value:/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message: 'el password debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.'
+                  }
+                })
+               }
               />
-            
+             <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
